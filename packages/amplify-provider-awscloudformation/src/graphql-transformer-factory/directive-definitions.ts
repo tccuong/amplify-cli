173f905299f1b644ed2ada49261063167c44093a
Alexander Harris <alharris@amazon.com>
2022-04-29 08:59:16 -0700

    chore: simplify transformer-factory entry point

--- packages/amplify-provider-awscloudformation/src/graphql-transformer-factory/directive-definitions.ts
+++ packages/amplify-provider-awscloudformation/src/graphql-transformer-factory/directive-definitions.ts
@@ -5,18 +5,15 @@ import {
   $TSContext,
 } from 'amplify-cli-core';
 import { print } from 'graphql';
-import { getTransformerFactoryV1, getTransformerFactoryV2 } from './transformer-factory';
-import { getTransformerVersion } from './transformer-version';
+import { getTransformerFactory } from './transformer-factory';
 
 /**
  * Return the set of directive definitions for the project, includes both appsync and amplify supported directives.
  * This will return the relevant set determined by whether or not the customer is using GQL transformer v1 or 2 in their project.
  */
 export async function getDirectiveDefinitions(context: $TSContext, resourceDir: string): Promise<string> {
-  const transformerVersion = await getTransformerVersion(context);
-  const transformList = transformerVersion === 2
-    ? await getTransformerFactoryV2(resourceDir)({ addSearchableTransformer: true, authConfig: {} })
-    : await getTransformerFactoryV1(context, resourceDir)(true);
+  const transformerFactory = await getTransformerFactory(context, resourceDir);
+  const transformList = await transformerFactory({ addSearchableTransformer: true, authConfig: {} })
 
   const transformDirectives = transformList
     .map(transformPluginInst => [transformPluginInst.directive, ...transformPluginInst.typeDefinitions].map(node => print(node)).join('\n'))
--- packages/amplify-provider-awscloudformation/src/graphql-transformer/api-utils.ts
+++ packages/amplify-provider-awscloudformation/src/graphql-transformer/api-utils.ts
@@ -1,24 +1,52 @@
-import { stateManager, getTransformerVersion, getGraphQLTransformerOpenSearchProductionDocLink } from "amplify-cli-core";
+import {
+  stateManager,
+  getTransformerVersion,
+  getGraphQLTransformerOpenSearchProductionDocLink,
+  getGraphQLTransformerAuthDocLink,
+  $TSContext,
+  $TSObject,
+} from "amplify-cli-core";
 import { printer } from "amplify-prompts";
 import { ResourceConstants } from "graphql-transformer-common";
 import _ from "lodash";
 
 export async function searchablePushChecks(context, map, apiName): Promise<void> {
-    const searchableModelTypes = Object.keys(map).filter(type => map[type].includes('searchable') && map[type].includes('model'));
-    if (searchableModelTypes.length) {
-      const currEnv = context.amplify.getEnvInfo().envName;
-      const teamProviderInfo = stateManager.getTeamProviderInfo();
-      const instanceType = _.get(
-        teamProviderInfo,
-        [currEnv, 'categories', 'api', apiName, ResourceConstants.PARAMETERS.ElasticsearchInstanceType],
-        't2.small.elasticsearch',
+  const searchableModelTypes = Object.keys(map).filter(type => map[type].includes('searchable') && map[type].includes('model'));
+  if (searchableModelTypes.length) {
+    const currEnv = context.amplify.getEnvInfo().envName;
+    const teamProviderInfo = stateManager.getTeamProviderInfo();
+    const instanceType = _.get(
+      teamProviderInfo,
+      [currEnv, 'categories', 'api', apiName, ResourceConstants.PARAMETERS.ElasticsearchInstanceType],
+      't2.small.elasticsearch',
+    );
+    if (instanceType === 't2.small.elasticsearch' || instanceType === 't3.small.elasticsearch') {
+      const version = await getTransformerVersion(context);
+      const docLink = getGraphQLTransformerOpenSearchProductionDocLink(version);
+      printer.warn(
+        `Your instance type for OpenSearch is ${instanceType}, you may experience performance issues or data loss. Consider reconfiguring with the instructions here ${docLink}`,
       );
-      if (instanceType === 't2.small.elasticsearch' || instanceType === 't3.small.elasticsearch') {
-        const version = await getTransformerVersion(context);
-        const docLink = getGraphQLTransformerOpenSearchProductionDocLink(version);
-        printer.warn(
-          `Your instance type for OpenSearch is ${instanceType}, you may experience performance issues or data loss. Consider reconfiguring with the instructions here ${docLink}`,
-        );
-      }
     }
   }
+}
+
+export const warnOnAuth = async (context: $TSContext, map: $TSObject): Promise<void> => {
+  const unAuthModelTypes = Object.keys(map).filter(type => !map[type].includes('auth') && map[type].includes('model'));
+  if (unAuthModelTypes.length) {
+    const transformerVersion = await getTransformerVersion(context);
+    const docLink = getGraphQLTransformerAuthDocLink(transformerVersion);
+    if (transformerVersion === 2) {
+      printer.info(
+        `
+  ⚠️  WARNING: Some types do not have authorization rules configured. That means all create, read, update, and delete operations are denied on these types:`,
+        'yellow',
+      );
+      printer.info(unAuthModelTypes.map(type => `\t - ${type}`).join('\n'), 'yellow');
+      printer.info(`Learn more about "@auth" authorization rules here: ${docLink}`, 'yellow');
+    } else {
+      context.print.warning("\nThe following types do not have '@auth' enabled. Consider using @auth with @model");
+      context.print.warning(unAuthModelTypes.map(type => `\t - ${type}`).join('\n'));
+      context.print.info(`Learn more about @auth here: ${docLink}\n`);
+    }
+  }
+};
