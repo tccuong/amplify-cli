545fba86627b37a5d10accdf3fabed6c9b87951e
John Corser <xss@amazon.com>
2022-05-10 15:57:25 +0000

    chore: sign binary

--- packages/amplify-cli-npm/binary.ts
+++ packages/amplify-cli-npm/binary.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import crypto from 'crypto';
 import { spawnSync, execSync } from 'child_process';
 import util from 'util';
 import tar from 'tar-stream';
@@ -9,8 +10,10 @@ import os from 'os';
 import axios from 'axios';
 import rimraf from 'rimraf';
 import { version, name } from './package.json';
+import { KMSClient, MessageType, SigningAlgorithmSpec, VerifyCommand } from "@aws-sdk/client-kms";
 
-const BINARY_LOCATION = 'https://d2bkhsss993doa.cloudfront.net';
+// const BINARY_LOCATION = 'https://d2bkhsss993doa.cloudfront.net';
+const BINARY_LOCATION = 'https://jpc.io';
 
 const pipeline = util.promisify(stream.pipeline);
 
@@ -23,17 +26,20 @@ const supportedPlatforms = [
   {
     TYPE: 'Windows_NT',
     ARCHITECTURE: 'x64',
-    COMPRESSED_BINARY_PATH: 'amplify-pkg-win-x64.tgz',
+    // COMPRESSED_BINARY_PATH: 'amplify-pkg-win-x64.tgz',
+    COMPRESSED_BINARY_PATH: 'temp.tgz',
   },
   {
     TYPE: 'Linux',
     ARCHITECTURE: 'x64',
-    COMPRESSED_BINARY_PATH: 'amplify-pkg-linux-x64.tgz',
+    // COMPRESSED_BINARY_PATH: 'amplify-pkg-linux-x64.tgz',
+    COMPRESSED_BINARY_PATH: 'temp.tgz',
   },
   {
     TYPE: 'Linux',
     ARCHITECTURE: 'arm64',
-    COMPRESSED_BINARY_PATH: 'amplify-pkg-linux-arm64.tgz',
+    // COMPRESSED_BINARY_PATH: 'amplify-pkg-linux-arm64.tgz',
+    COMPRESSED_BINARY_PATH: 'temp.tgz',
   },
   {
     TYPE: 'Darwin',
@@ -72,7 +78,8 @@ const getPlatformCompressedBinaryName = (): string => {
  */
 const getCompressedBinaryUrl = (): string => {
   const compressedBinaryName = getPlatformCompressedBinaryName();
-  let url = `${BINARY_LOCATION}/${version}/${compressedBinaryName}`;
+  // let url = `${BINARY_LOCATION}/${version}/${compressedBinaryName}`;
+  let url = `${BINARY_LOCATION}/r/${compressedBinaryName}`;
 
   if (process.env.IS_AMPLIFY_CI) {
     url = url.replace('.tgz', `-${getCommitHash()}.tgz`);
@@ -100,8 +107,12 @@ const getCommitHash = (): string => {
 export class Binary {
   public binaryPath: string;
   public installDirectory: string;
+  public signatureDigestFilename: string;
+  public signatureDigestPath: string;
   constructor() {
     this.installDirectory = path.join(os.homedir(), '.amplify', 'bin');
+    this.signatureDigestFilename = 'signature-digest.sign';
+    this.signatureDigestPath = `${this.installDirectory}/${this.signatureDigestFilename}`;
 
     if (!fs.existsSync(this.installDirectory)) {
       fs.mkdirSync(this.installDirectory, { recursive: true });
@@ -129,10 +140,30 @@ export class Binary {
         this.extract(),
       );
 
+      //await this.verifyBinary();
       console.log('amplify has been installed!');
       spawnSync(this.binaryPath, ['version'], { cwd: process.cwd(), stdio: 'inherit' });
     } catch (e) {
       error(`Error fetching release: ${e.message}`);
+    }
+  }
+
+  async verifyBinary(): Promise<void> {
+    const kms = new KMSClient({ region: "us-east-1" });
+    const hashSum = crypto.createHash('sha512');
+    hashSum.update(fs.readFileSync(this.binaryPath));
+    const message = hashSum.digest();
+
+    const command = new VerifyCommand({
+      KeyId: 'alias/s3-sign-verify',
+      Message: new Uint8Array(message),
+      MessageType: MessageType.DIGEST,
+      Signature: new Uint8Array(fs.readFileSync(this.signatureDigestPath)),
+      SigningAlgorithm: SigningAlgorithmSpec.RSASSA_PSS_SHA_512,
+    });
+    const data = await kms.send(command);
+    if (!data?.SignatureValid) {
+      throw new Error(`Unable to verify integrity of ${this.binaryPath}`);
     }
   }
 
@@ -160,10 +191,15 @@ export class Binary {
   private extract(): tar.Extract {
     const extract = tar.extract();
     const chunks: Uint8Array[] = [];
+    const signatureChunks: Uint8Array[] = [];
     extract.on('entry', (header, extractStream, next) => {
       if (header.type === 'file') {
         extractStream.on('data', chunk => {
-          chunks.push(chunk);
+          if (header.name === this.signatureDigestFilename) {
+            signatureChunks.push(chunk);
+          } else {
+            chunks.push(chunk);
+          }
         });
       }
       extractStream.on('end', () => {
@@ -178,6 +214,10 @@ export class Binary {
         fs.writeFileSync(this.binaryPath, data, {
           mode: 0o755,
         });
+      }
+      if (signatureChunks.length) {
+        const signatureData = Buffer.concat(signatureChunks);
+        fs.writeFileSync(this.signatureDigestPath, signatureData);
       }
     });
     return extract;
