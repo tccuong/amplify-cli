4921388d3077336d9f9a2c337e0380ddbf6b8a0a
Alexander Harris <alharris@amazon.com>
2022-05-11 16:46:55 -0700

    chore: flip to using module.exports and the providerPlugin factory methods

--- packages/amplify-cli-core/src/plugin-facade/api-category-facade.ts
+++ packages/amplify-cli-core/src/plugin-facade/api-category-facade.ts
@@ -1,10 +1,15 @@
 // import { DeploymentResources as DeploymentResourcesV2 } from '@aws-amplify/graphql-transformer-core';
 // import { DeploymentResources as DeploymentResourcesV1 } from 'graphql-transformer-core';
+import { TransformerPluginProvider as TransformerPluginProviderV2 } from '@aws-amplify/graphql-transformer-interfaces';
+import { ITransformer as TransformerPluginProviderV1 } from 'graphql-transformer-core';
 // eslint-disable-next-line import/no-cycle
 import {
-//   $TSAny,
+  $TSAny,
   $TSContext,
 } from '..';
+
+const API_CATEGORY_NAME = 'api';
+const PROVIDER_NAME = 'awscloudformation';
 
 /**
  * Facade for the API Category, to facilitate typed requests against some of the plugin methods exposed.
@@ -14,14 +19,27 @@ export class ApiCategoryFacade {
    * Get the transformer version used in this project.
    */
   static async getTransformerVersion(context: $TSContext): Promise<number> {
-    return context.amplify.invokePluginMethod(context, 'api', undefined, 'getTransformerVersion', [context]);
+    return context.amplify.invokePluginMethod(context, API_CATEGORY_NAME, undefined, 'getTransformerVersion', [context]);
   }
 
   /**
    * Return the supported transformer directives.
    */
   static async getDirectiveDefinitions(context: $TSContext, resourceDir: string): Promise<string> {
-    return context.amplify.invokePluginMethod(context, 'api', undefined, 'getDirectiveDefinitions', [context, resourceDir]);
+    return context.amplify.invokePluginMethod(context, API_CATEGORY_NAME, undefined, 'getDirectiveDefinitions', [context, resourceDir]);
+  }
+
+  /**
+   * Return the transformer factory, will return the relevant factory based on whether the project
+   * is using v2 or v1 transformers.
+   */
+  static async getTransformerFactory(
+    context: $TSContext,
+    resourceDir: string,
+    authConfig?: $TSAny,
+  ): Promise<(options: $TSAny) => Promise<(TransformerPluginProviderV2 | TransformerPluginProviderV1)[]>> {
+    const providerPlugin = await import(context.amplify.getProviderPlugins(context)[PROVIDER_NAME]);
+    return providerPlugin.getTransformerFactory(context, resourceDir, authConfig);
   }
 
 //   /**
